9dc813008dc15f84c6fcafd5d6e93361eb079d1b
1like2learn <ndespained@gmail.com>
2020-10-02 08:56:17 -0700

    exp2 done and commented

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,21 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    route = []
+    # For every ticket store it's key = source and value = destination
+    for ticket in tickets:
+        cache[ticket.source] = ticket.destination
 
+    # Check every link in the chain of locations until the destination 
+    # is "NONE." Append places to the route list in the order they are 
+    # in the chain.
+    place = cache["NONE"]
+    while not place == "NONE":
+        route.append(place)
+        place = cache[place]
+        
+    # Add None because that needs to be on the end of the list for 
+    # some reason.
+    route.append(place)
     return route
