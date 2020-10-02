4230768994fcabd6c1f24653d36515ea50662520
1like2learn <ndespained@gmail.com>
2020-10-02 08:25:45 -0700

    ex1 done and commented

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,15 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
-
+    cache = {}
+    
+    # For every weight in weights check if limit minus the current
+    # value is in the chache. If it is return it in the appropriate 
+    # touple. Otherwise add the current value to the cache.
+    for i, weight in enumerate(weights):
+        if (limit - weight) in cache:
+            return (i,cache[limit - weight])
+        else:
+            cache[weight] = i
+    # If we don't find a good pair return none
     return None
